08261b0ebea8dee33cc79b730fb7f7ae712965c1
chan990416 <ssr@xcrypt.co.kr>
2025-03-13 14:57:30 +0900

    add btn2 (clear) and handler function

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class calculator(QWidget):
         self.btn1 = QPushButton('Massage',self)
         self.btn1.clicked.connect(self.activateMessage)
 
+        self.btn2 = QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addStretch(1)
+        #vbox.addStretch(1)
         vbox.addWidget(self.btn1)
+        vbox.addWidget(self.btn2)
         vbox.addStretch(1)
         self.setLayout(vbox)
         self.setWindowTitle('Calculator')
@@ -32,6 +37,9 @@ class calculator(QWidget):
     def activateMessage(self):
         #QMessageBox.information(self,"information","Button_clecked")
         self.te1.appendPlainText("Button Clicked")
+
+    def clearMessage(self):
+        self.te1.clear()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
