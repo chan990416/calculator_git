e77d8e2cac78c9e0c19b40a4ab36c7695aade18d
chan990416 <ssr@xcrypt.co.kr>
2025-03-13 12:20:01 +0900

    add icon.png file and push button with handler function in the main .py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
 
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout,QMessageBox
+from PyQt5.QtGui  import QIcon
 
 class calculator(QWidget):
     def __init__(self):
@@ -10,10 +11,21 @@ class calculator(QWidget):
 
 
     def initUI(self):
+        self.btn1 = QPushButton('Massage')
+        self.btn1.clicked.connect(self.activateMessage)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        self.setLayout(vbox)
         self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+
         self.resize(512,512)
         self.show()
-
+    def activateMessage(self):
+        QMessageBox.information(self,"information","Button_clecked")
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
